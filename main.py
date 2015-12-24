4b4452d0a9cb744366b71e61ebd91dea5144f610
Chris Bogaards <chrisbogaards.cb@gmail.com>
2015-12-24 13:27:36 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+items = input("On what continent do you live?\n")
+if items.lower() == "europe":
+    print('That\'s nice, I live there to! I live in Holland.\nMaybe we can meet one time.')
+else:
+    print('Too bad you live in ' + items + ', I live in Holland, Europe.\nYou should come visit Europe one time.\n Europe is really nice!')
